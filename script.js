14360b2335d1172e6bc5300d3e0301b91a523d64
Jeanne-Bsb <98830120+Jeanne-Bsb@users.noreply.github.com>
2023-07-24 14:21:18 +0200

    upgrade profils add

--- script.js
+++ script.js
@@ -1,77 +1,97 @@
-/* import "./mail.js"; */
-
 //création de champs pour chaque intervenants
 const containerAll = document.querySelector(".intervenants");
 const nbInterv = document.getElementById("nbInterv");
 let containers = [];
 nbInterv.addEventListener("change", () => {
     for (let i = 0; i < nbInterv.value; i++) {
-        console.log(i);
         if (document.querySelector(".intervenant" + i) === null) {
-            containers[i] = document.createElement("div");
-            containers[i].className = "intervenant" + i;
-
-            //création des titres
-            let titres = document.createElement("h3");
-            titres.innerHTML = "Intervenant n°" + (i + 1);
-            containers[i].appendChild(titres);
-
-            //création du label nom prénom
-            let nomLabel = document.createElement("label");
-            nomLabel.htmlFor = "nom" + i;
-            nomLabel.innerHTML = "Prénom - Nom:";
-            containers[i].appendChild(nomLabel);
-
-            //création du champs nom prénom
-            let nomInput = document.createElement("input");
-            nomInput.type = "text";
-            nomInput.name = "nom" + i;
-            nomInput.id = "nom" + i;
-            containers[i].appendChild(nomInput);
-
-            //création du label fonction
-            let fonctionLabel = document.createElement("label");
-            fonctionLabel.htmlFor = "fonction" + i;
-            fonctionLabel.innerHTML = "Fonction et département:";
-            containers[i].appendChild(fonctionLabel);
-
-            //création du champs fonction
-            let fonctionInput = document.createElement("textarea");
-            fonctionInput.name = "fonction" + i;
-            fonctionInput.id = "fonction" + i;
-            fonctionInput.cols = "30";
-            fonctionInput.rows = "10";
-            containers[i].appendChild(fonctionInput);
-
-            //création du label lien
-            let lienLabel = document.createElement("label");
-            lienLabel.htmlFor = "linknom" + i;
-            lienLabel.innerHTML = "Lien vers la fiche annuaire :";
-            containers[i].appendChild(lienLabel);
-
-            //création du champs lien
-            let lienInput = document.createElement("input");
-            lienInput.type = "text";
-            lienInput.name = "linknom" + i;
-            lienInput.id = "linknom" + i;
-            containers[i].appendChild(lienInput);
-
-            containerAll.appendChild(containers[i]);
+            let newInterv =
+                `<div class="intervenant` +
+                i +
+                ` intervContainer">
+            <h3>Intervenant n°` +
+                (i + 1) +
+                `</h3>
+            <div>
+                <label for="nom` +
+                i +
+                `">Prénom - Nom:</label>
+                <input type="text" name="nom` +
+                i +
+                `" id="nom` +
+                i +
+                `" placeholder="Arthur">
+            </div>
+            <div>
+                <label for="linknom` +
+                i +
+                `">Lien vers la fiche annuaire :</label>
+                <input type="text" name="linknom` +
+                i +
+                `" id="linknom` +
+                i +
+                `" placeholder="https://annuaire.fr/page-">
+            </div>
+            <div>
+                <label for="fonction` +
+                i +
+                `">Fonction et département :</label>
+                <textarea name="fonction` +
+                i +
+                `" id="fonction` +
+                i +
+                `" cols="30" rows="10"></textarea>
+            </div>
+            </div>`;
+            containerAll.innerHTML += newInterv;
         }
     }
-    //masquage des champs en trop
-    containers.forEach((container, index) => {
-        if (index >= nbInterv.value) {
-            container.style.display = "none";
-        }
-    });
+    document
+        .querySelectorAll(".intervContainer")
+        .forEach((container, index) => {
+            //masquage des champs en trop
+            if (index >= nbInterv.value) {
+                container.style.display = "none";
+            }
+            //réafichage les champs en plus
+            if (index < nbInterv.value) {
+                container.style.display = "block";
+            }
+        });
 });
 
 modifTemplate = (e) => {
-    e.preventDefault(); //permet de prévenir l’envoi du formulaire au serveur.???? what is that?
-    console.log(e.target.id, e.target.value);
+    e.preventDefault();
+    let stringAdd = e.target.value;
 
-    document.querySelector("#view-" + e.target.id).innerHTML = e.target.value;
+    //cas particuliers :
+    //date
+    if (e.target.id === "date") {
+        let date = e.target.value.split("-");
+        const month = [
+            "janvier",
+            "février",
+            "mars",
+            "avril",
+            "mai",
+            "juin",
+            "juillet",
+            "août",
+            "septembre",
+            "octobre",
+            "novembre",
+            "décembre",
+        ];
+        stringAdd = date[2] + " " + month[date[1] - 1] + " " + date[0];
+    }
+    //heures
+    else if (e.target.id === "hstart" || e.target.id === "hend") {
+        stringAdd = e.target.value.replace(":", "h");
+    }
+    //liens
+    else if (e.target.id === "linknom") {
+    }
+    document.querySelector("#view-" + e.target.id).innerHTML = stringAdd;
 };
 
 //récupération des valeurs des champs au clique du bouton
@@ -85,7 +105,7 @@ let editor = document.querySelector(".view");
 let button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    console.log(editor);
+    /* console.log(editor); */
     /* editor.select(); */
     navigator.clipboard.writeText(editor);
     button.innerText = "Copié !";
