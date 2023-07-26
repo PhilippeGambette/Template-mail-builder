cb4f138c3bd667d71c9a06bf5ae7bec6ecea4a9f
Jeanne-Bsb <98830120+Jeanne-Bsb@users.noreply.github.com>
2023-07-26 10:36:35 +0200

    upgrade of links and guest
    :white_check_mark:  the add of links in the view works
    :white_check_mark:  when we add a guest, it add it in the view
    :negative_squared_cross_mark: the events on guest input doesn't work well

--- script.js
+++ script.js
@@ -44,8 +44,27 @@ nbInterv.addEventListener("change", () => {
             </div>
             </div>`;
             containerAll.innerHTML += newInterv;
+
+            let newIntervView =
+                `<div style="text-align: center; width: 33%;"><img src="http://besombes.butmmi.o2switch.site/rdv_data_dsos/icon.svg" alt="" />
+            <p><strong> <a href="" id="view-linknom` +
+                i +
+                `"><span id="view-nom` +
+                i +
+                `"></span id="view-nom1"></a></strong><br /><span id="view-fonction` +
+                i +
+                `"></span></p>
+            </div>`;
+            document.querySelector("#intervenants").innerHTML += newIntervView;
+            document
+                .querySelector("#nom" + i)
+                .addEventListener("keyup", modifField);
+            document
+                .querySelector("#nom" + i)
+                .addEventListener("change", modifField);
         }
     }
+    //masquage et affichage des champs dans le formulaire
     document
         .querySelectorAll(".intervContainer")
         .forEach((container, index) => {
@@ -58,16 +77,55 @@ nbInterv.addEventListener("change", () => {
                 container.style.display = "block";
             }
         });
+    //masquage et affichage des éléments dans le corps du mail
+    document
+        .querySelectorAll("#intervenants div")
+        .forEach((container, index) => {
+            //masquage des éléments en trop
+            if (index >= nbInterv.value) {
+                container.style.display = "none";
+            }
+            //réafichage les champs en plus
+            if (index < nbInterv.value) {
+                container.style.display = "block";
+            }
+        });
 });
 
-modifTemplate = (e) => {
-    e.preventDefault();
-    let stringAdd = e.target.value;
+testId = (id) => {
+    let debut = "";
+    for (let i = 0; i < 4; i++) {
+        debut += id[i];
+    }
+    if (debut === "link") {
+        return true;
+    } else {
+        return false;
+    }
+};
+modifField = (e) => {
+    modifTemplate(e.target.id);
+    if (e.target.id === "nom1") {
+        console.log("ça appele la fonction");
+    }
+};
+
+modifFields = () => {
+    document.querySelectorAll(".event-target").forEach((field) => {
+        modifTemplate(field.id);
+    });
+};
+
+modifTemplate = (id) => {
+    /* e.preventDefault(); */ /* 
+    console.log(id); */
+    let valeur = document.getElementById(id).value;
+    let stringAdd = valeur;
 
     //cas particuliers :
     //date
-    if (e.target.id === "date") {
-        let date = e.target.value.split("-");
+    if (id === "date") {
+        let date = valeur.split("-");
         const month = [
             "janvier",
             "février",
@@ -85,28 +143,30 @@ modifTemplate = (e) => {
         stringAdd = date[2] + " " + month[date[1] - 1] + " " + date[0];
     }
     //heures
-    else if (e.target.id === "hstart" || e.target.id === "hend") {
-        stringAdd = e.target.value.replace(":", "h");
+    if (id === "hstart" || id === "hend") {
+        stringAdd = valeur.replace(":", "h");
     }
     //liens
-    else if (e.target.id === "linknom") {
+    if (testId(id)) {
+        document.getElementById("view-" + id).href = valeur;
+    } else {
+        document.querySelector("#view-" + id).innerHTML = stringAdd;
     }
-    document.querySelector("#view-" + e.target.id).innerHTML = stringAdd;
 };
 
+window.addEventListener("load", modifFields);
 //récupération des valeurs des champs au clique du bouton
-document.querySelectorAll("input,textarea").forEach((input) => {
-    input.addEventListener("keyup", modifTemplate);
-    input.addEventListener("change", modifTemplate);
+document.querySelectorAll(".event-target").forEach((input) => {
+    input.addEventListener("keyup", modifField);
+    input.addEventListener("change", modifField);
 });
 
 //bouton copier
-let editor = document.querySelector(".view");
+/* let editor = document.querySelector(".view");
 let button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    /* console.log(editor); */
-    /* editor.select(); */
     navigator.clipboard.writeText(editor);
     button.innerText = "Copié !";
 });
+ */
